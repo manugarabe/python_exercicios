8352dd95803e4c4c6e4b0a5c7b3700a8d9c1504b
manugarabe <manugarabe@gmail.com>
2025-03-13 22:18:01 -0300

    Delete lista5ex3.py

--- lista5ex3.py
+++
@@ -1,7 +0,0 @@
-valor_emprestimo = float(input('Insira o valor do empréstimo: '))
-valor_salario = float(input('Insira o valor do salário: '))
-
-if valor_emprestimo > valor_salario * 0.20:
-    print('Empréstimo não será concedido.')
-else:
-    print('Empréstimo concedido.')
