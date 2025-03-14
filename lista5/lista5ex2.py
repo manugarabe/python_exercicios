1c476cd898e90cdbe928e6795bd43857bef3d601
manugarabe <manugarabe@gmail.com>
2025-03-13 21:59:18 -0300

    Delete lista5 directory

--- lista5/lista5ex2.py
+++
@@ -1,7 +0,0 @@
-n1 = float(input('Insira o primeiro número: '))
-n2 = float(input('Insira o segundo número: '))
-
-if n1 > n2:
-    print('O primeiro número é o maior:', n1)
-else:
-    print('O segundo número é o maior:', n2)
--- lista5/lista5ex6.py
+++
@@ -1,9 +0,0 @@
-preco = float(input('Insira o preço do carro: '))
-ano = int(input('Insira o ano do carro: '))
-
-if ano < 1990:
-    taxa = 0.01 * preco
-    print(f'O imposto a ser pago é: {taxa:,.2f}')
-else:
-    taxa = 0.015 * preco
-    print(f'O imposto a ser pago é: {taxa:,.2f}')
